c61ad31fd5255c4180dabe1fbdd93521bd41b5c9
thebuilderekes <ekeopreberedugo1@gmail.com>
2023-04-19 20:54:58 +0100

    updated learning topics

--- call-bind-apply.js
+++ call-bind-apply.js
@@ -1,8 +1,32 @@
+
+// ---call() and bind() are used to set this keywords by ourselves
+
+// ---call() can be used to call a method of one object with a function of another object
+//call() can take in two arguments, the object you want to bind "this" to and the value of the parameter.
+
+//apply() works ust like call, the only difference is that it takes the second argument in an array
+
+//eg 1
+
+const cat = {
+  name: "Bailey"
+}
+
+
+function sayHello(message){
+  console.log(`${this.name} says ${message}`)
+}
+
+sayHello.call(cat, "nice to meet you" )  //output: Bailey says nice to meet you
+sayHello.apply(cat, ["nice to meet you"] )  //output: Bailey says nice to meet you
+
+
+//eg 2
 const person = {
   firstname: "ekeopre",
   lastname: "beredugo",
   getFullName: function () {
-    const fullname = `${this.firstname}  ${this.lastname}`;
+    const fullname = `${this.firstname}  ${this.lastname}`
 
     return fullname;
   },
@@ -13,8 +37,14 @@ const logName = function () {
   // .bind creates a copy of whatever function you pass it to.
 };
 
-//   const logPersonName = logName.bind(person)
+  const logPersonName = logName.bind(person);
 
-logName.call(person); // Using .call seems like a better alternative to bind in the sense that you can use this directly on the function that contains "this" keyword and pass the object that you are referring to in the parenthesis.
+//but with call() it is much easier.
+logName.call(person); // We are setting our new value of "this" as "person" by using the .call(person). 
+//console.log(`logged: ${this.getFullName()}`); will be translate as console.log(`logged: ${person.getFullName()}`);
+
+//IMPORTANT
+//Using .call() is a better alternative to bind in the sense that you can use this directly on the function that contains "this" keyword , pass the object that you are referring to in the parenthesis and have the function called immediately.
 
 //.apply is a way of passing all the properties of one object into another oobject by using .apply
+
